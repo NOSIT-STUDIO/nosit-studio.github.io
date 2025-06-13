5572a7046b951c21e05a8fcaa554a4aabe632933
JustDeveloper <176615419+JustDeveloper1@users.noreply.github.com>
2025-06-13 16:56:15 +0300

    Update just.config.js

--- just.config.js
+++ just.config.js
@@ -2,6 +2,12 @@ module.exports = {
     type: "generator",
     docs_config: {
         metatitle: "a",
-        domain: "nosit-studio.github.io"
+        domain: "nosit-studio.github.io",
+        links: [
+            ["a", "/a", "_self"], ["_just", "https://just.is-a.dev/", "_blank"]
+        ],
+        buttons: [
+            ["creator", "https://justdeveloper.is-a.dev/", "_blank"]
+        ]
     }
 }
